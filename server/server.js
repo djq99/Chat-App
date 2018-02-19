0469a003b6e471377f33f3c4699f996a130190cb
Jiaqi Duan <imdjq1990@gmail.com>
2018-02-18 22:40:03 -0800

    Create new user, and alert others

--- server/server.js
+++ server/server.js
@@ -15,10 +15,15 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
   console.log('New user connected');
 
-  socket.emit('newEmail',{
-    from:'mike@example.com',
-    text:'Hey. What is going on.',
-    createAt: 123
+  socket.emit('newMessage',{
+    from:'Admin',
+    text:'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+  socket.broadcast.emit('newMessage',{
+    from:'Admin',
+    text:'New user joined',
+    createdAt: new Date().getTime()
   });
 
   socket.on('createMessage',(message)=>{
@@ -28,6 +33,11 @@ io.on('connection',(socket)=>{
       text:message.text,
       createAt:new Date().getTime()
     });
+    // socket.broadcast.emit('newMessage',{
+    //   from:message.from,
+    //   text:message.text,
+    //   createdAt:new Date().getTime()
+    // })
   })
 
   socket.on('disconnect',()=>{
