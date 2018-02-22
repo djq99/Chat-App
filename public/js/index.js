43eff1b10526b11e5bef6bc6bd5b6c8aa9784598
Jiaqi Duan <imdjq1990@gmail.com>
2018-02-21 21:29:15 -0800

    Add mustache.js for meesage template

--- public/js/index.js
+++ public/js/index.js
@@ -10,21 +10,27 @@ socket.on('disconnect',function (){
 
 
 socket.on('newMessage',function(message){
-  const formattedTime = moment(message.createdAt).format('h:mm a');
-  var li = jQuery('<li></li>');
-  li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
-  jQuery('#messages').append(li);
+  const formattedTime = moment(message.createdAt).format('h:mm a');
+  const template = jQuery('#message-template').html();
+  const html = Mustache.render(template,{
+    text:message.text,
+    from:message.from,
+    createdAt:formattedTime
+  });
+
+  jQuery('#messages').append(html);
 })
 
 socket.on('newLocationMessage',function(message){
   const formattedTime = moment(message.createdAt).format('h:mm a');
-  var li = jQuery('<li></li>');
-  var a = jQuery('<a target="_blank">My current location</a>');
-  li.text(`${message.from} ${formattedTime}: `);
-  a.attr('href',message.url);
-  li.append(a);
-  jQuery('#messages').append(li);
+  const template = jQuery('#location-message-template').html();
+  const html = Mustache.render(template,{
+    url:message.url,
+    from:message.from,
+    createdAt:formattedTime
+  });
+  jQuery('#messages').append(html);
 })
 
 
